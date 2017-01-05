3d229eac64ddbcf0a624780e369c4006ea111c41
wanghao <hao.wang@capitalonline.net>
2017-01-05 11:01:47 +0800

    运行脚本

---
+++ bin/run.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+cat >> /usr/local/apache2/conf/httpd.conf <<EOF
+ServerName localhost:8001
+LoadModule wsgi_module modules/mod_wsgi.so
+AddType text/html .py
+WSGIScriptAlias / /data/wechat/wh/wsgi.py
+WSGIPythonPath /data/wechat/wh/
+<Directory /data/wechat/wh>
+    <Files wsgi.py>
+        Require all granted
+    </Files>
+</Directory>
+Alias /wechat/static/ /data/wechat/wechat/static/
+<Directory /data/wechat/wechat/static/>
+    Require all granted
+</Directory>
+
+EOF
+service httpd start
+
+chmod -R 777 /data/wechat/logs/
+
+tail -f /var/log/messages
+
+
+
