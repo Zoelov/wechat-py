bd194c7b98f50b44bf471639fdf0a290575652eb
wanghao <hao.wang@capitalonline.net>
2017-01-09 16:31:22 +0800

    脚本

--- bin/run.sh
+++ bin/run.sh
@@ -17,8 +17,8 @@ Alias /wechat/static/ /data/wechat/wechat/static/
 
 EOF
 service httpd start
-cd /data/wechat/wechat/
-nohup python user.py &
+#cd /data/wechat/wechat/
+#nohup python user.py &
 
 chmod -R 777 /data/wechat/logs/
 
